1535ab7cbb68a174834452c955b99f8a63e804e8
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2024-03-08 11:22:26 +0900

    update usage.py

--- usage.py
+++ usage.py
@@ -9,27 +9,26 @@ sAPI = SwitchbotAPI(
     secret=""
 )
 
-all_devices = sAPI.devices
-
 print("All devices")
+all_devices = sAPI.devices
 pprint(all_devices)
 
 devices = sAPI.get_devices(["X00X0000000X"])
 plug_mini: PlugMiniJP = devices["X00X0000000X"]
 plug_status: PlugMiniJPStatus = sAPI.status(plug_mini)
 
-print("Status:", plug_mini)
-print(f"  {plug_status}")
-print(f"    deviceId={plug_status.device_id}")
-print(f"    hubDeviceId={plug_status.hub_device_id}")
-print(f"    voltage={plug_status.voltage}")
-print(f"    version={plug_status.version}")
-print(f"    weight={plug_status.weight}")
-print(f"    electricityOfDay={plug_status.electricity_of_day}")
-print(f"    electricCurrent={plug_status.electric_current}")
+print("\nStatus:", plug_mini)
+print(f"  └ {plug_status}")
+print(f"      ├ deviceId={plug_status.device_id}")
+print(f"      ├ hubDeviceId={plug_status.hub_device_id}")
+print(f"      ├ voltage={plug_status.voltage}")
+print(f"      ├ version={plug_status.version}")
+print(f"      ├ weight={plug_status.weight}")
+print(f"      ├ electricityOfDay={plug_status.electricity_of_day}")
+print(f"      └ electricCurrent={plug_status.electric_current}")
 
 
-print("Turn on")
+print("\nTurn on")
 sAPI.run(plug_mini.command_turn_on())
 
 time.sleep(3)
